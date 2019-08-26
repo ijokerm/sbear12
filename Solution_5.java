70ab0f71bf392ca94d1583e0ecb408dc677786b3
ijokerm <2013099087@qq.com>
2019-08-26 19:55:52 +0800

    二叉树层序遍历

---
+++ Solution_5.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class Solution_5 {
+    public static void preOrderf(Node root){
+        Stack<Node>stack=new Stack<>();
+        Node cur=root;
+        while(!stack.empty()||cur!=null){
+            while(cur!=null){}
+        }
+    }
+}
